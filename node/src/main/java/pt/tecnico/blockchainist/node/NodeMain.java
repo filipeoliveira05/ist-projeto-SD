c5680ea33351cce68b408c4b917931ca5fad8518
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2026-02-27 10:01:08 +0000

    feat(node): setup and start gRPC server in NodeMain

--- node/src/main/java/pt/tecnico/blockchainist/node/NodeMain.java
+++ node/src/main/java/pt/tecnico/blockchainist/node/NodeMain.java
@@ -1,7 +1,31 @@
 package pt.tecnico.blockchainist.node;
 
+import java.io.IOException;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import pt.tecnico.blockchainist.node.domain.NodeState;
+
 public class NodeMain {
-    public static void main(String[] args) {
-        // TODO
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println(NodeMain.class.getSimpleName());
+
+        // check arguments
+        if (args.length < 1) {
+            System.err.println("Argument(s) missing!");
+            System.err.printf("Usage: java %s <port>%n", NodeMain.class.getName());
+            return;
+        }
+
+        final int port = Integer.parseInt(args[0]);
+        final NodeState nodeState = new NodeState();
+        final NodeServiceImpl nodeService = new NodeServiceImpl(nodeState);
+
+        Server server = ServerBuilder.forPort(port).addService(nodeService).build();
+
+        server.start();
+        System.out.println("Server started, listening on " + port);
+
+        server.awaitTermination();
     }
 }
